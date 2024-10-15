92d1891dd5f3206770185b338aa554eee20d0b20
kamizh <alzar2508@gmail.com>
2024-10-15 17:03:22 +0500

    Добавьте файлы проекта.

---
+++ Zaripov_car_service/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Zaripov_car_service
+{
+    class Manager
+    {
+        public static Frame MainFrame {get;set; }
+    }
+}
